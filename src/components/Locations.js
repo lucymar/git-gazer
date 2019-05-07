882ce91e3a14897c82e558b09675b722916e661c
lucymar <lucygibb68@gmail.com>
2019-05-06 20:14:46 -0400

    changes map api

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Map from './Map';
 
 function Locations(props) {
   return (
@@ -13,6 +14,7 @@ function Locations(props) {
           ))}
         </ul>
       </div>
+      <Map />
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,50 +1,70 @@
 import React, { Component } from 'react';
-import { ApolloConsumer } from 'react-apollo';
-import gql from 'graphql-tag';
-import Loading from './Loading';
-import { Query } from 'react-apollo';
-import Map from 'mapbox-gl';
-const mapboxgl = require('mapbox-gl');
-//const buildMarker = require('./marker.js');
+import ReactMapGL from 'react-map-gl';
 
-// const GET_DATA = gql`
-//   {
-//     repository(owner: "facebook", name: "create-react-app") {
-//       forks(last: 5) {
-//         edges {
-//           node {
-//             owner {
-//               login
-//               ... on User {
-//                 location
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
+class Map extends Component {
+  state = {
+    viewport: {
+      width: 400,
+      height: 400,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8,
+    },
+  };
 
-// export const Map1 = ({ onLocationGot }) => (
-//   <Query query={GET_DATA}>
-//     {({ loading, error, data }) => {
-//       if (loading) return 'loading...';
-//       if (error) return `Error! ${error.message}`;
-//       console.log('this is being sentn from the map component', data);
-//       return <div>data goes here</div>;
-//     }}
-//   </Query>
-// );
+  render() {
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        mapboxApiAccessToken={
+          'pk.eyJ1IjoibHVjeW1hciIsImEiOiJjanQ0ajFoeWMwZDEwNDRwaGlwM2F3M3A2In0.uHXTEtSC78BHunbaDzaDFA'
+        }
+        onViewportChange={viewport => this.setState({ viewport })}
+        className="map"
+      />
+    );
+  }
+}
+export default Map;
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoibHVjeW1hciIsImEiOiJjanQ0ajFoeWMwZDEwNDRwaGlwM2F3M3A2In0.uHXTEtSC78BHunbaDzaDFA';
+// // const GET_DATA = gql`
+// //   {
+// //     repository(owner: "facebook", name: "create-react-app") {
+// //       forks(last: 5) {
+// //         edges {
+// //           node {
+// //             owner {
+// //               login
+// //               ... on User {
+// //                 location
+// //               }
+// //             }
+// //           }
+// //         }
+// //       }
+// //     }
+// //   }
+// // `;
 
-export const MyMap = new mapboxgl.Map({
-  container: 'map',
-  center: [0, 0], //
-  zoom: 0, // starting zoom
-  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
-});
+// // export const Map1 = ({ onLocationGot }) => (
+// //   <Query query={GET_DATA}>
+// //     {({ loading, error, data }) => {
+// //       if (loading) return 'loading...';
+// //       if (error) return `Error! ${error.message}`;
+// //       console.log('this is being sentn from the map component', data);
+// //       return <div>data goes here</div>;
+// //     }}
+// //   </Query>
+// // );
 
-export default MyMap;
+// mapboxgl.accessToken =
+//   'pk.eyJ1IjoibHVjeW1hciIsImEiOiJjanQ0ajFoeWMwZDEwNDRwaGlwM2F3M3A2In0.uHXTEtSC78BHunbaDzaDFA';
+
+// export const MyMap = new mapboxgl.Map({
+//   container: 'map',
+//   center: [0, 0], //
+//   zoom: 0, // starting zoom
+//   style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+// });
+
+// export default MyMap;
